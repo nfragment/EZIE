b30d6ce45e86e0ffc8261db2f2a05b2a65468aef
nfragment <helpdesk@nfragment.com>
2024-02-22 21:45:05 +0530

    Fix for using script on Linux

--- autoconf/gen.py
+++ autoconf/gen.py
@@ -3,12 +3,18 @@ import os
 from collections import defaultdict
 from zipfile import ZipFile, ZIP_STORED
 
-RAW_DIR = '.\\raw'
+RAW_DIR = './raw' # use this for linux
+# RAW_DIR = '.\\raw' # use this for windows
 packages = defaultdict(list)
 
+print ("starting stage")
+
 for board_dir, _, files in os.walk(RAW_DIR):
+    print ("entered for loop")
     if files := [f for f in files if not f.startswith('.')]:
-        a, _, arch, board = board_dir.split('\\')
+        print ("executing if block")
+        a, _, arch, board = board_dir.split('/') # use this for linux
+        # a, _, arch, board = board_dir.split('\\') # use this for windows
         os.makedirs(arch, exist_ok=True)
         print(f"Compressing {board} for {arch}")
 
@@ -20,7 +26,9 @@ for board_dir, _, files in os.walk(RAW_DIR):
                 myzip.write(filepath, file)
         packages[arch].append(board)
 
-
+print ("intermediate stage")
 for arch, packages_list in packages.items():
     with open(f"{arch}_manifest.json", "w") as manifest:
         json.dump({"files": sorted(packages_list, key=str.casefold)}, manifest, indent=None, separators=(",", ":"))
+
+print ("final stage")
